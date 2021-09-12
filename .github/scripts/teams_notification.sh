e90f4b800eca674c22dae5457bf6a408d80ef9ad
Constantin Krüger <Constantinidis@web.de>
2021-09-13 00:33:42 +0200

    use script

--- .github/scripts/teams_notification.sh
+++ .github/scripts/teams_notification.sh
@@ -6,7 +6,7 @@ send_msg () {
   \"@context\": \"https://schema.org/extensions\",
   \"@type\": \"MessageCard\",
   \"title\": \"&#x1F4E2; ${RELEASE_TITLE}\",
-  \"text\": \"Release: ${1}\",
+  \"text\": \"Release: ${RELEASE_TEXT}\",
   \"potentialAction\": [
     {
       \"@type\": \"OpenUri\",
